138df431ffb378e8c23e5fed33fd394931e0170c
Ubuntu <ubuntu@ip-172-31-3-57.ec2.internal>
2017-04-29 06:00:59 +0000

    Update worker

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from kafka import KafkaProducer
 import os
 import random
+import time
 import pdb
 
 app = Flask(__name__)
@@ -12,11 +13,13 @@ files = []
 
 @app.route('/simple', methods=['POST'])
 def simple():
-        return write_to_queue('simple1')
+        filename = write_to_queue('simple1')
+        return wait_and_rm(filename, 'simple')
 
 @app.route('/complex', methods=['POST'])
 def complex():
-        return write_to_queue('complex1')
+        filename = write_to_queue('complex1')
+        return wait_and_rm(filename, 'complex')
 
 def write_to_queue(queue):
         filename = random.choice(files)
@@ -24,11 +27,26 @@ def write_to_queue(queue):
         data = fh.read()
         fh.close()
         producer.send(queue, key=bytes(filename), value=bytes(data))
-        return '200'
+        return filename
 
-    
+def wait_and_rm(filename, req_type):
+        filepath = os.path.join('/home/ubuntu/cs672-server-performance/files/', req_type, filename)
+
+        attempts = 0
+        while True:
+                if os.path.exists(filepath):
+                        print 'unlink %s' % filepath
+                        os.unlink(filepath)
+                        return '200'
+
+                if attempts > 100:
+                        return '504'
+                
+                attempts += 1
+                time.sleep(.1)
+
 if __name__ == '__main__':
-        for (dirpath, dirnames, filenames) in os.walk('/etc'):
+        for (dirpath, dirnames, filenames) in os.walk('files/etc'):
                 files.extend([os.path.join(dirpath, fil) for fil in filenames])
 
         app.run(host='0.0.0.0')
--- worker.py
+++ worker.py
@@ -1,32 +1,29 @@
-import threading, logging, time, os
-
+import threading
+import logging
+import time
+import os
+import hashlib
+import binascii
+    
 from kafka import KafkaConsumer, KafkaProducer
 
 KAFKA_CONNECTION = os.environ.get('KAFKA_CONNECTION')
+ROUNDS = 700000
 
-producer = None
+producer = KafkaProducer(bootstrap_servers=KAFKA_CONNECTION)
 
-class SimpleProducer(threading.Thread):
-    daemon = True
-
-    def __init__(self):
-        producer = KafkaProducer(bootstrap_servers=KAFKA_CONNECTION)
-    
-    def send(self, key, value, queue):
-        producer.send(queue, key=bytes(key), value=bytes(value))
-            
 class SimpleConsumer(threading.Thread):
+
     daemon = True
                 
     def run(self):
+
         consumer = KafkaConsumer(bootstrap_servers=KAFKA_CONNECTION)
         consumer.subscribe(['simple1'])
 
-        for message in consumer:
-            print 'simple write: ' + message.key
-            fh = open(os.path.join('/home/ubuntu/files/simple/', message.key), 'w')
-            fh.write(message.value)
-            fh.close()
+        while True:
+            for message in consumer:
+                write_file(message.key, message.value, 'simple')
 
 class ComplexConsumer(threading.Thread):
     daemon = True
@@ -34,24 +31,63 @@ class ComplexConsumer(threading.Thread):
     def run(self):
         consumer = KafkaConsumer(bootstrap_servers=KAFKA_CONNECTION)
         consumer.subscribe(['complex1'])
-        
-        for message in consumer:
-            producer.send(message.key, message.value, 'complex2')
 
+        for message in consumer:
+            data = bytes(message.value)
+            use_some_cpu(data)
+            producer.send('complex2', key=bytes(message.key), value=data)
+
+class ComplexConsumer2(threading.Thread):
+    daemon = True
+                
+    def run(self):
+        consumer = KafkaConsumer(bootstrap_servers=KAFKA_CONNECTION)
+        consumer.subscribe(['complex2'])
+
+        for message in consumer:
+            data = bytes(message.value)
+            use_some_cpu(data)
+            producer.send('complex3', key=bytes(message.key), value=data)
+
+class ComplexConsumer3(threading.Thread):
+    daemon = True
+                
+    def run(self):
+        consumer = KafkaConsumer(bootstrap_servers=KAFKA_CONNECTION)
+        consumer.subscribe(['complex3'])
+
+        while True:
+            for message in consumer:
+                data = bytes(message.value)
+                use_some_cpu(data)
+                write_file(message.key, message.value, 'complex')
+
+def use_some_cpu(data):
+    dk = hashlib.pbkdf2_hmac('sha256', data, b'saltysaltsalt', ROUNDS)
+    return binascii.hexlify(dk)
+                
+def write_file(filename, data, req_type):
+    path = os.path.join('/home/ubuntu/cs672-server-performance/files/', req_type, os.path.dirname(filename))
+    print 'write file: %s' % os.path.join(path, os.path.basename(filename))
+    if not os.path.exists(path):
+        os.makedirs(path)
+        
+    fh = open(os.path.join(path, os.path.basename(filename)), 'w')
+    fh.write(data)
+    fh.close()
+    
             
 def main():
-
-    producer = SimpleProducer()
-    
     threads = [
-        #Producer(),
         SimpleConsumer(),
-        ComplexConsumer()
+        ComplexConsumer(),
+        ComplexConsumer2(),
+        ComplexConsumer3()
     ]
 
     for t in threads:
         t.start()
-
+        
     while True:
         time.sleep(10)
 
